d8e3a08101e07907a7cfb5d2583303fb239c6282
Connor Ashton <cjconnorashton@gmail.com>
2023-09-14 15:41:07 -0600

    added types

--- client/src/utils/store.ts
+++ client/src/utils/store.ts
@@ -1,6 +1,13 @@
 import { create } from "zustand";
 
-export const useLectureStore = create((set) => ({
+export type BearsState = {
+  title: string;
+  class: string;
+  changeTitle: (name: string) => void;
+  changeClass: (name: string) => void;
+};
+
+export const useLectureStore = create<BearsState>((set) => ({
   title: "",
   class: "",
   changeTitle: (name: string) => set(() => ({ title: name })),
